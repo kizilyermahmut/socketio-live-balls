2974b1367e7b0c1ee53a406eedfcc1ecd57c7585
mahmut <mkiziyern@gmail.com>
2022-01-29 23:20:03 +0300

    ng-cloak

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,134 @@
-app.controller('indexController', ( 'indexFactory', ($scope, indexFactory) => {
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+    /*  
+    burada mantık sistemi scope üzerinden yakalamaak
+    $scope.messages =[{
+        text: 'selam',
+    },
+    {
+        text: 'merhaba'
+    }];*/
+    $scope.messages = [ ];
+    $scope.players ={ };
     
-    const connectionOptions ={
-        reconnectionAttempts: 3,
-        reconnectionDelay: 600
+    $scope.init=()=>{
+        const username = prompt('Please enter username');
+        if (username)
+        initSocket(username);
+        else
+        return false;
+        
+    };
+    function scrollTop() {
+    setTimeout(() => {
+        //mesaj sonrası scrool aşağı indirme
+            const element = document.getElementById('chat-area');
+            element.scrollTop=element.scrollHeight; 
+        });
+    };
+
+    function showBubble(id, message) {
+        $('#'+ id).find('.message').show().html(message);
+        setTimeout(() => {
+            $('#'+ id).find('.message').hide();
+        }, 2000);
+    }
+    
+    function initSocket(username){
+        const connectionOptions ={
+            reconnectionAttempts: 3,
+            reconnectionDelay: 600
+        };
+        
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+        
+        .then((socket) =>{
+            socket.emit('newUser', {username})
+            socket.on('initPlayers', (players) => {
+                $scope.players = players;
+                $scope.$apply();
+            })
+            socket.on('newUser', (data) => {
+                const messageData = {
+                    type:{
+                        code: 0, //server or user message
+                        message:1
+                    }, //giriş info veriyor diğerlerine
+                    username: data.username
+                };
+                
+                
+                $scope.messages.push(messageData);
+                $scope.players[data.id] = data;
+                scrollTop();
+                $scope.$apply();
+            })
+            socket.on('disUser', (data) => {
+                const messageData = {
+                    type:{
+                        code: 0, //server or user message
+                        message:0 // login or disconnect message
+                    }, //giriş info veriyor diğerlerine
+                    username: data.username
+                };
+                
+                $scope.messages.push(messageData);
+                delete $scope.players[data.id];
+                scrollTop();
+                $scope.$apply();
+            });
+            
+            socket.on('animate', data => { 
+                $('#' + data.socketId).animate({'left':data.x, 'top': data.y}, () =>{
+                    animate = false
+                });
+            });
+            let animate = false;
+            $scope.onClickPlayer = ($event) => {
+                
+                if(!animate){
+                    let x = $event.offsetX;
+                    let y = $event.offsetY;
+                    socket.emit('animate', {x, y});
+                    animate = true;
+                    $('#' + socket.id).animate({'left':x, 'top': y}, () =>{
+                        animate = false
+                    })        
+                }
+                
+                
+            };
+
+            $scope.newMessage = () => {
+                let message = $scope.message;
+                
+                const messageData = {
+                    type:{
+                        code: 1, //server or user message
+                        
+                    }, //giriş info veriyor diğerlerine
+                    username: username,
+                    text: message
+                };
+            
+            $scope.messages.push(messageData);
+            $scope.message = ''
+            
+            socket.emit('newMessage', messageData);
+            showBubble(socket.id, message);
+            scrollTop();
+
+            }
+            socket.on('newMessage', message => {
+                $scope.messages.push(message);
+                $scope.$apply();
+                showBubble(message.socketId, message.text);
+                scrollTop();
+            })
+         
+        }).catch((err) => {
+            console.log(err);
+        });
+        
     };
     
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-     
-    .then((socket) =>{
-        console.log('bağlantı gerçekleşti', socket);
-    }).catch((err) => {
-        console.log(err);
-    });
-}));
+}]);    
--- src/socketAPI.js
+++ src/socketAPI.js
@@ -2,12 +2,70 @@
 const socketio = require('socket.io');
 const io = socketio();
 
-const socketApi = { };
 
+
+
+const socketApi = { };
+const users = { };// array yerine object yaparak user.id ile bilgileri 
+//işlemi yapabiliriz.
+
+const randomColor = require('../helpers/randomColor');
 socketApi.io = io;
+
+
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('newUser', (data) =>{
+       // console.log(data);
+       const defaultData = {
+           id:socket.id,
+           position:{
+               x:0,
+               y:0
+           },
+           color: randomColor()
+       };
+   
+    //new userdan gelen bilgi ile default data
+    //işlemini object.assign ile birleştirmesi gercekleştirilebilir.
+    const userData = Object.assign(data, defaultData);
+    users[socket.id] = userData;
+    console.log(users);
+    
+    socket.broadcast.emit('newUser', users[socket.id]);
+    socket.emit('initPlayers', users);
+    
+    });
+
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('disUser', users[socket.id])
+        delete users[socket.id];
+     
+    })
+    socket.on('animate', (data) => {
+        try {
+        users[socket.id].position.x=data.x;
+        users[socket.id].position.y=data.y;
+        
+        socket.broadcast.emit('animate',{
+            socketId:socket.id,
+            x: data.x,
+            y: data.y
+        });
+        }catch(err){
+            console.log(err)
+        }
+    })
+
+    socket.on('newMessage', data =>{
+        const messageData = Object.assign({socketId:socket.id}, data);
+        socket.broadcast.emit('newMessage', messageData);
+    })
 });
+
+
+
 
 module.exports = socketApi;
